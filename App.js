458ade496e578b9960c49fd4382375b505115900
gapsong <khiemtonthat@yahoo.de>
2018-09-15 15:53:56 +0200

    ready for a merge conflict

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Hello worlddoch nicht!</Text>
+        <Text>Ich rase aus!</Text>
         <Text>Shake your phone to open the developer menu.</Text>
       </View>
     );
